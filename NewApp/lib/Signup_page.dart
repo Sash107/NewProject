9d8364cd01078ac2cb10c6ac87ef3e7c0a8aaabb
Suyash Upadhyay <suyashupadhyay2004@gmail.com>
2024-10-05 02:37:47 +0530

    commit

---
+++ NewApp/lib/Signup_page.dart
@@ -0,0 +1,384 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _FirstNameController = TextEditingController();
+  TextEditingController _MiddleNameController = TextEditingController();
+  TextEditingController _LastNameController = TextEditingController();
+  TextEditingController _NickNameController = TextEditingController();
+  TextEditingController _PhoneNumberController = TextEditingController();
+  TextEditingController _EmailAddressController = TextEditingController();
+  TextEditingController _PasswordController = TextEditingController();
+  TextEditingController _PermanentAddressController = TextEditingController();
+  TextEditingController _CurrentAddressController = TextEditingController();
+  TextEditingController _ContactNoController = TextEditingController();
+  TextEditingController _TypeOfSocietyController = TextEditingController();
+  TextEditingController _NameOfSocietyController = TextEditingController();
+  TextEditingController _BlockController = TextEditingController();
+  TextEditingController _FlatController = TextEditingController();
+
+  String _FirstName = '';
+  String _MiddleName = '';
+  String _LastName = '';
+  String _NickName = '';
+  String _PhoneNumber = '';
+  String _EmailAddress = '';
+  String _Password = '';
+  String _PermanentAddress = '';
+  String _CurrentAddress = '';
+  String _ContactNo = '';
+  String _TypeOfSociety = '';
+  String _NameOfSociety = '';
+  String _Block = '';
+  String _Flat = '';
+  bool _isLoading = false;
+  bool _obscureText = true;
+  Future<void> _signup() async {
+
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    print("First Name: ${_FirstNameController.text}");
+    print("Last Name: ${_LastNameController.text}");
+    print("Mobile: ${_PhoneNumberController.text}");
+    print("Email: ${_EmailAddressController.text}");
+    print("Address: ${_CurrentAddressController.text}");
+
+    final url = 'http://192.168.207.130:8000/signup';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'FirstName': _FirstName,
+          'MiddleName': _MiddleName,
+          'LastName':_LastName,
+          'Nickname': _NickName,
+          'MobileNumber': _PhoneNumber,
+          'Email': _EmailAddress,
+          'CurrentAddress': _CurrentAddress,
+          'PermanentAddress': _PermanentAddress,
+          'EmergencyNumber': _ContactNo,
+          'Password': _Password,
+          'ConfirmPassword': _Password,
+          'SocietyName': _NameOfSociety,
+          'Block': _Block,
+          'FlatNumber': _Flat,
+        }),
+      );
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(responseBody['Status'])),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Signup failed: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      print('Error occurred: $e');
+    }
+
+
+    setState(() {
+      _isLoading = false;
+    });
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Color(0xFF0F52BA),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Create An Account",
+                style: TextStyle(
+                    color: Color(0xFF0F52BA),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0,left: 5),
+                      child: TextFormField(
+                          controller: _FirstNameController,
+                        decoration: InputDecoration(
+                            labelText: "First Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: TextFormField(
+                        controller: _MiddleNameController,
+                        decoration: InputDecoration(
+                            labelText: "Middle Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+        
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0,left: 5),
+                      child: TextFormField(
+                        controller: _LastNameController,
+                        decoration: InputDecoration(
+                            labelText: "Last Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: TextFormField(
+                        controller: _NickNameController,
+                        decoration: InputDecoration(
+                            labelText: "Nick Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0,left: 5),
+                      child: TextFormField(
+                        controller: _PhoneNumberController,
+                        decoration: InputDecoration(
+                            labelText: "Phone Number",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: TextFormField(
+                        controller: _EmailAddressController,
+                        decoration: InputDecoration(
+                            labelText: "E-mail Address",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _PasswordController,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _PermanentAddressController,
+                  decoration: InputDecoration(
+                      labelText: "Permanent Address",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _CurrentAddressController,
+                  decoration: InputDecoration(
+                      labelText: "Current Address",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _ContactNoController,
+                  decoration: InputDecoration(
+                      labelText: "Contact No.",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _TypeOfSocietyController,
+                  decoration: InputDecoration(
+                      labelText: "Type of Society",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0,right: 20),
+                child: TextFormField(
+                  controller: _NameOfSocietyController,
+                  decoration: InputDecoration(
+                      labelText: "Name Of The Society",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0,left: 5),
+                      child: TextFormField(
+                        controller: _BlockController,
+                        decoration: InputDecoration(
+                            labelText: "Block/Tower",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: TextFormField(
+                        controller: _FlatController,
+                        decoration: InputDecoration(
+                            labelText: "Flat/Villa Number",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10,right: 10),
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(Color(0xFF0F52BA))),
+                      onPressed: () {
+                        setState(() {
+                          _FirstName = _FirstNameController.text;
+                          _MiddleName = _MiddleNameController.text;
+                          _LastName = _LastNameController.text;
+                          _NickName = _NickNameController.text;
+                          _PhoneNumber = _PhoneNumberController.text;
+                          _EmailAddress = _EmailAddressController.text;
+                          _Password = _PasswordController.text;
+                          _PermanentAddress = _PermanentAddressController.text;
+                          _CurrentAddress = _CurrentAddressController.text;
+                          _ContactNo = _ContactNoController.text;
+                          _TypeOfSociety = _TypeOfSocietyController.text;
+                          _NameOfSociety = _NameOfSocietyController.text;
+                          _Block = _BlockController.text;
+                          _Flat = _FlatController.text;
+                        });
+                        _signup();
+                      },
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ NewApp/lib/signin_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Signup_page.dart';
+import 'mainhome.dart';
+
+class Signinpage extends StatefulWidget {
+  const Signinpage({super.key});
+
+  @override
+  State<Signinpage> createState() => _SigninpageState();
+}
+
+class _SigninpageState extends State<Signinpage> {
+
+  TextEditingController _EmailController = TextEditingController();
+  TextEditingController _PasswordController = TextEditingController();
+
+  String _Email = '';
+  String _Password = '';
+
+
+  @override
+  void dispose() {
+    _EmailController.dispose();
+    _PasswordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          backgroundColor: Color(0xFF0F52BA)),
+      body: SingleChildScrollView(
+        child: Form(
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 10),
+                child: Text(
+                  "Sign In To Your Existing "
+                  "Account",
+                  style: TextStyle(
+                      color: Color(0xFF0F52BA),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                controller: _EmailController,
+                decoration: InputDecoration(
+                    labelText: "E-mail",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _PasswordController,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(height: 30),
+              Container(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              WidgetStatePropertyAll(Color(0xFF0F52BA,))),
+                      onPressed: () {
+                        setState(() {
+                          _Email = _EmailController.text;
+                          _Password = _PasswordController.text;
+                        });
+                        Navigator.push(context, MaterialPageRoute(builder: (context) {
+                           return Home();
+                           },));
+                      },
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(color: Colors.white),
+                      ))),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(onPressed: () {
+
+              }, child: Text("Forgot Password??",style:TextStyle(color: Colors.black))),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Or",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 70,
+                    width: 150,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                           backgroundColor: WidgetStatePropertyAll(Color(0xFF0F52BA))),
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Image.asset("image/google.webp",width: 30,height: 30,),
+                            SizedBox(width: 10,),
+                            Text("Google",style: TextStyle(color: Colors.white),)
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  SizedBox(
+                    height: 70,
+                    width: 150,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(Color(0xFF0F52BA))),
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Image.asset("image/facebook.webp",width: 30,height: 30,),
+                            SizedBox(width: 5,),
+                            Text("Facebook",style: TextStyle(color: Colors.white),)
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return SignUp();
+                      },
+                    ));
+                  },
+                  child: Text("Create New Account!",style:TextStyle(color: Colors.black))),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
